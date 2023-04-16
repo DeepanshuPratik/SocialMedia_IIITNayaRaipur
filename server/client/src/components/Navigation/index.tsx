90a0f2235df5c3191d756badb82549d03976cded
Soumya Ranjan Patnaik <soumyaranjan1812@gmail.com>
2023-04-16 20:50:43 +0530

    Changes for nonstop demo

--- server/client/src/components/Navigation/index.tsx
+++ server/client/src/components/Navigation/index.tsx
@@ -1,165 +1,140 @@
-import React, { useEffect, useState } from 'react';
-import { useLocation, useNavigate } from 'react-router';
-import { Link } from 'react-router-dom';
-import navLinks from '../../data/navLinks';
-import useAppContext from '../../hooks/useAppContext';
-import Logo from '../Icons/Logo';
-import NightIcon from '../Icons/NightIcon';
-import SunIcon from '../Icons/SunIcon';
-
-interface SingleLinkProps extends React.HTMLAttributes<HTMLDivElement> {
-  name: string;
-  active?: boolean;
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useCookies } from "react-cookie";
+import { NavLink, useLocation } from "react-router-dom";
+export const baseUrl = "https://nonstop.sranjan.co│n.in/nonstop_api";
+interface NavLinkProps {
+  children: React.ReactNode;
+  to: string;
 }
-export default function Navigation() {
-  const location = useLocation();
-  const [currActive, setActive] = useState(0);
-  const [tempActive, setTempActive] = useState(currActive);
-  useEffect(() => {
-    navLinks.forEach((item, index) => {
-      if (location.pathname === '/app/' + item) {
-        setActive(index);
-      }
-    });
-  }, [location.pathname]);
-  useEffect(() => {
-    setTempActive(currActive);
-  }, [currActive]);
+
+const CustomLink = ({ to, children }: NavLinkProps) => {
+  const {pathname: path} = useLocation();
+  const isActive = path === to || path === `/${to}`;
+  const className = `text-sm ${
+    isActive
+      ? "text-green-600 font-bold"
+      : "text-gray-600 hover:text-gray-600 dark:text-gray-300"
+  } `;
   return (
-    <nav
-      className="flex justify-between fixed top-0 w-full bg-background_variant 
-    dark:bg-d-background_variant text-primary dark:text-d-primary text-sm h-11 px-2 py-2 z-50"
-    >
-      <NavLeft />
-      <div
-        id="nav_mid"
-        className="flex "
-        onMouseLeave={() => {
-          setTempActive(currActive);
-        }}
+    <NavLink className={className} to={to}>
+      {children}
+    </NavLink>
+  );
+};
+
+export interface User {
+  id: string,
+  img: string,
+  createdAt: string,
+  name: string,
+  updatedAt: string,
+  email: string
+}
+
+
+export const Navbar = () => {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [,, removeCookie] = useCookies(['connect.sid']);
+
+  const [user, setUser] = useState<any>()
+
+  useEffect(() => {
+    axios.get(`${baseUrl}/account`).then((res) => {
+      setIsLoggedIn(res.data.loggedIn);
+      const user: User = res.data
+      setUser(user)
+      window.localStorage.setItem("user", JSON.stringify(user))
+    });
+  }, []); 
+
+  const logout = () => {
+    axios.post(`${baseUrl}/logout`).then(() => {
+    setIsLoggedIn(false)
+    })
+    removeCookie('connect.sid')
+  }  
+
+  return (
+    <nav className="relative px-8 h-16 flex justify-between items-center border-y border-gray-400 dark:border-gray-700">
+      {/* Home */}
+      <NavLink
+        className="text-3xl font-bold leading-none flex items-center space-x-4"
+        to="#"
       >
-        {navLinks.map((item, index) => {
-          return (
-            <SingleLink
-              name={item}
-              key={index}
-              active={index === tempActive}
-              onMouseEnter={() => {
-                setTempActive(index);
-              }}
+        <span>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="#03fc3d"
+            className="w-8 h-8"
+          >
+            <path
+              fillRule="evenodd"
+              d="M9.315 7.584C12.195 3.883 16.695 1.5 21.75 1.5a.75.75 0 01.75.75c0 5.056-2.383 9.555-6.084 12.436A6.75 6.75 0 019.75 22.5a.75.75 0 01-.75-.75v-4.131A15.838 15.838 0 016.382 15H2.25a.75.75 0 01-.75-.75 6.75 6.75 0 017.815-6.666zM15 6.75a2.25 2.25 0 100 4.5 2.25 2.25 0 000-4.5z"
+              clipRule="evenodd"
             />
-          );
-        })}
+            <path d="M5.26 17.242a.75.75 0 10-.897-1.203 5.243 5.243 0 00-2.05 5.022.75.75 0 00.625.627 5.243 5.243 0 005.022-2.051.75.75 0 10-1.202-.897 3.744 3.744 0 01-3.008 1.51c0-1.23.592-2.323 1.51-3.008z" />
+          </svg>
+        </span>
+        <span className="text-gray-600 dark:text-gray-300 text-xl">
+          NonStop
+        </span>
+      </NavLink>
+
+      <div className="lg:hidden">
+        <button className="navbar-burger flex items-center text-gray-600 dark:text-gray-300 p-3">
+          <svg
+            className="block h-4 w-4 fill-current"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <title>Mobile menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z"></path>
+          </svg>
+        </button>
       </div>
-      <NavRight />
+
+      <ul className="hidden lg:flex lg:items-center grow mx-10 space-x-6">
+        {/* Home */}
+        <li>
+          <CustomLink to={""}>Home</CustomLink>
+        </li>
+        <li>
+          <CustomLink to={"mentorship"}>Mentorship</CustomLink>
+        </li>
+        <li>
+          <CustomLink to={"learn"}>Learn</CustomLink>
+        </li>
+        <li>
+          <CustomLink to={"competitions"}>Competitions</CustomLink>
+        </li>
+      </ul>
+      <div className="hidden lg:block">
+        {isLoggedIn ? (
+          <div className="flex items-center space-x-8">
+            <img
+              className="inline-block w-12 h-12 rounded-full"
+              src={user.img}
+              alt="John Doe"
+            />
+            <span className="flex flex-col">
+              <span className="text-sm font-medium text-gray-900 dark:text-gray-100">
+                {user.name}
+              </span>
+              <CustomLink to={"profile"}><span className="text-sm font-medium text-gray-500 dark:text-gray-400 cursor-pointer">
+                View Profile
+              </span></CustomLink>
+            </span>
+            <button className="bg-green-600 px-4 py-2 rounded-full" onClick={_ => logout()}>Logout</button>
+          </div>
+        ) : (
+          <NavLink to={`${baseUrl}/auth/google`}><button className="bg-green-600 px-4 py-2 rounded-full">Signup with Goggle</button></NavLink>
+        )}
+      </div>
     </nav>
   );
-}
-
-const NavLeft: React.FC = () => {
-  return (
-    <div
-      id="nav_left"
-      className="flex items-center gap-2 h-full border-l-transparent border-y-transparent border-r-hints dark:border-r-d-hints 
-      pr-3"
-      style={{ borderWidth: '1px' }}
-    >
-      <Logo className="w-8" />
-      <span className="hidden sm:inline text-lg font-semibold ">
-        Social IIITNR
-      </span>
-    </div>
-  );
 };
 
-const NavRight: React.FC = () => {
-  const { darkMode, setDarkMode, setIsModalOpen, setModalChildren } =
-    useAppContext();
-  const nav = useNavigate();
-  const handleClick = () => {
-    if (setDarkMode) setDarkMode((curr) => !curr);
-  };
-
-  // Sign Out Confirmation Component for Modal Child
-  // Clear Local Storage if yes
-  const HangleSignOut: React.FC = () => {
-    return (
-      <div className="flex flex-col justify-center items-center gap-4">
-        <h2 className="text-center text-primary font-medium text-sm">
-          Are you sure you want to sign out?
-        </h2>
-        <div className="flex justify-center gap-2">
-          <button
-            className="btn flex justify-center items-center text-xs capitalize py-2 w-24 text-white"
-            onClick={() => {
-              setIsModalOpen(false);
-              localStorage.clear();
-              nav('/login');
-            }}
-          >
-            Yes
-          </button>
-          <button
-            className="btn flex justify-center items-center text-xs capitalize py-2 w-24 text-white"
-            onClick={() => {
-              setIsModalOpen(false);
-            }}
-          >
-            No
-          </button>
-        </div>
-      </div>
-    );
-  };
-
-  const signOut = () => {
-    // Open Modal and set child to SignOutModal
-    setIsModalOpen(true);
-    setModalChildren(<HangleSignOut />);
-  };
-
-  return (
-    <div id="nav_right" className="h-full flex  gap-4 ">
-      <button
-        className=" border-x-hints border-y-transparent"
-        style={{ borderWidth: '1px' }}
-        onClick={handleClick}
-      >
-        {darkMode ? (
-          <SunIcon className="w-6 mx-3" />
-        ) : (
-          <NightIcon className="w-6 mx-3" />
-        )}
-      </button>
-      <button
-        className="flex justify-center items-center rounded-lg h-full p-1 outline-primary
-         dark:outline-d-primary outline-2 outline focus:text-text-primary  focus:bg-primary_variant
-          hover:text-text-primary hover:bg-primary_variant hover:border-0"
-        onClick={signOut}
-      >
-        Sign Out
-      </button>
-    </div>
-  );
-};
-
-const SingleLink: React.FC<SingleLinkProps> = ({
-  name,
-  active,
-  onMouseEnter,
-}) => {
-  return (
-    <div
-      className="group flex flex-col capitalize overflow-hidden px-2"
-      onMouseEnter={(e) => onMouseEnter?.call(this, e)}
-    >
-      <Link to={`/app/${name.toLowerCase()}`}>{name}</Link>
-      <div
-        className={`transform transition-transform h-0.5 w-full origin-left ${
-          !active && 'scale-x-0'
-        } group-hover:scale-x-100 mt-0.5 bg-primary dark:bg-d-primary rounded-full `}
-      />
-    </div>
-  );
-};
-
+export default Navbar;
